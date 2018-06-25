7ddb20daa53d14027076b8f788158687c9b17c77
Ricardo Hernández <ricardohs35@gmail.com>
2018-06-25 11:27:35 -0500

    ✨ Create ApiController to handle all responses

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ApiController extends Controller {
+    protected $statusCode = 200;
+
+    /**
+     * @return mixed
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * @param mixed $statusCode
+     * @return $this
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+
+        return $this;
+    }
+
+    public function respondNotFound($message = 'Not Found!')
+    {
+        return $this->setStatusCode(404)->respondWithError($message);
+    }
+
+    public function respond($data, $headers = [])
+    {
+        return response()->json($data, $this->getStatusCode(), $headers);
+    }
+
+    public function respondWithError($message)
+    {
+        return $this->respond([
+            'error' => [
+                'message' => $message,
+                'status_code' => $this->getStatusCode()
+            ]
+        ]);
+    }
+}
