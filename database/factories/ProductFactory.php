fa6f4ad6f35f9564c3add55422c94d83dfada930
Ricardo Hernández <ricardohs35@gmail.com>
2018-06-24 21:14:53 -0500

    ✨ Create Migration, Factory and Seeder for Products

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Product::class, function (Faker $faker) {
+    return [
+        'code' => $faker->domainWord. ' ' . rand(1, 100),
+        'description' => $faker->realText,
+        'price' => $faker->randomFloat(2, 100, 9999)
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Product;
 
 class DatabaseSeeder extends Seeder
 {
+    private $tables = ['products'];
+
     /**
      * Seed the application's database.
      *
@@ -11,6 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->truncate_all_tables();
+        $this->call(ProductsTableSeeder::class);
         // $this->call(UsersTableSeeder::class);
+    }
+
+    private function truncate_all_tables ()
+    {
+        foreach ($this->tables as $table) {
+            \DB::table($table)->truncate();
+        }
     }
 }
