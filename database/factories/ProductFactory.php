d76f33d33ae93d626464598894a577b776a3ef2f
Ricardo Hernández <ricardohs35@gmail.com>
2018-06-25 10:45:19 -0500

    💥 Add active field to products

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,6 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
         'code' => $faker->domainWord. ' ' . rand(1, 100),
+        'active' => $faker->boolean(),
         'description' => $faker->realText,
         'price' => $faker->randomFloat(2, 100, 9999)
     ];
--- database/migrations/2018_06_25_011050_create_products_table.php
+++ database/migrations/2018_06_25_011050_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('code')->unique();
+            $table->boolean('active')->default(0);
             $table->string('description');
             $table->decimal('price', 6, 2);
             $table->timestamps();
