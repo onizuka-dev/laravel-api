80ef3dedc58c98d24a1eed5bb29040bb82cb1e4c
Ricardo Hernández <ricardohs35@gmail.com>
2018-06-29 16:54:52 -0500

    ✨ Update migrations

--- database/migrations/2018_06_28_212714_create_taxes_table.php
+++ database/migrations/2018_06_28_212714_create_taxes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
+class CreateTaxesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('taxes', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('category_id');
-            $table->string('code')->unique();
-            $table->boolean('active')->default(0);
             $table->string('description');
-            $table->decimal('price', 6, 2);
+            $table->float('percentage');
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('taxes');
     }
 }
