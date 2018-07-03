22af4bb471b8d3c3e87afaf4daad464cba78565f
Ricardo Hernández <ricardohs35@gmail.com>
2018-07-02 23:44:26 -0500

    🎨 Update Product migration, make category_id default to 0

--- database/migrations/2018_06_25_011050_create_products_table.php
+++ database/migrations/2018_06_25_011050_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('category_id');
+            $table->integer('category_id')->unsigned()->default(0);
             $table->string('code')->unique();
             $table->boolean('active')->default(0);
             $table->string('description');
